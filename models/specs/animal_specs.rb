14912b9ac6b0321aeb526dc6e821a9692f8c990d
Ray <ray.greenan.89@outlook.com>
2018-11-09 13:48:41 +0000

    added basic test functions to animal

--- models/specs/animal_specs.rb
+++ models/specs/animal_specs.rb
@@ -1,13 +1,27 @@
 require("minitest/autorun")
 require_relative("../animal")
 
-# class TestAnimal < MiniTest::Test
-#
-#   def setup
-#     options = {"id" => "", "name" => "Lossoth", "type" => "Labrador",
-#       "admission_date" => "21/06/18"}
-#
-#     @animal = Animal.new(options)
-#   end
-#
-# end
+class TestAnimal < MiniTest::Test
+
+  def setup
+    options = {"id" => 1, "name" => "Lossoth", "type" => "Labrador",
+      "admission_date" => "21/06/18"}
+
+    @animal = Animal.new(options)
+  end
+
+  def test_name
+    result = @animal.name
+    assert_equal("Lossoth", result)
+  end
+
+  def test_type
+    result = @animal.type
+    assert_equal("Labrador", result)
+  end
+
+  def test_admission_date
+    result = @animal.admission_date
+    assert_equal("21/06/18", result)
+  end
+end
